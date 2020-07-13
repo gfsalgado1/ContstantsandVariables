87e2b1f7abe14be50f9b5b7ff64fa74cebe92fe9
gfsalgado1 <52936092+gfsalgado1@users.noreply.github.com>
2020-07-13 15:00:26 -0500

    Add files via upload

---
+++ Giselle_S_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+// ADD
+var number1 = 2
+var number2 = 3
+print("Your answer is : \(number1 + number2)")
+
+//SUBTRACT
+var number3 = 4
+var number4 = 8
+print("Your answer is: \(number4 - number3)")
+
+
+print("The numbers mulitplied together are: \(number1 * number3)")
+
+print("The numbers divided together are: \(number2 / number4)")
